77f83ab5ad6fc0c521073d0bc219589663bbaf02
n-sakuma <n.sakuma@everyleaf.com>
2011-08-08 18:39:01 +0900

    from PHP to Ruby

--- omikujigohan.rb
+++ omikujigohan.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'erb'
+require './menu'
 
 get '/' do
   erb :index
+end
+
+post '/result' do
+  menulist = params.select{|k, v| (k =~ /^item\d+$/) && !(v.empty?) }.values
+  @menu = Menu.new(menulist.empty? ? nil : menulist)
+  erb :result
 end
